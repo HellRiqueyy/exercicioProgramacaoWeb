be96f007e3d63686d06f56153cefb9b73afccabe
HellRiqueyy <rique.silva.moura@gmail.com>
2025-09-16 20:37:26 -0300

    primeiro commit | exercicio 1

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const EURO = 5.7;
+const DOLAR = 5.2;
+
+let real = 20.0;
+
+console.log("Dinheiro: "+real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
+console.log("Real para Euro: "+(EURO/real).toLocaleString('de-DE', { style: 'currency', currency: 'EUR' }));
+console.log("Real para Dolar: "+(DOLAR/real).toLocaleString('en-US', { style: 'currency', currency: 'USD' }))
+
