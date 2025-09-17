fac1be19a44c87b62c8ba833026a9928d1e46b00
HellRiqueyy <rique.silva.moura@gmail.com>
2025-09-16 21:20:30 -0300

    segundo commit | exercicio 2

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-
+//Exercicio 1
 const EURO = 5.7;
 const DOLAR = 5.2;
 
 let real = 20.0;
 
-console.log("Dinheiro: "+real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
-console.log("Real para Euro: "+(EURO/real).toLocaleString('de-DE', { style: 'currency', currency: 'EUR' }));
-console.log("Real para Dolar: "+(DOLAR/real).toLocaleString('en-US', { style: 'currency', currency: 'USD' }))
+console.log("Dinheiro: " + real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
+console.log("Real para Euro: " + (EURO / real).toLocaleString('de-DE', { style: 'currency', currency: 'EUR' }));
+console.log("Real para Dolar: " + (DOLAR / real).toLocaleString('en-US', { style: 'currency', currency: 'USD' }))
 
+//Exercicio 2
+
+let idade = 20
+let cartao = true;
+let responsavel = true;
+
+if ((idade >= 18 && cartao) || responsavel) {
+    console.log("Entrada permitida!")
+} else {
+    console.log("Entrada negada!")
+}
